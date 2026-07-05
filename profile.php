ea388c3ceecca38e25bb15935837dae99443fded
James <jmsrt123@gmail.com>
2026-07-05 11:34:51 +0700

    modified:   profile.php

--- profile.php
+++ profile.php
@@ -11,7 +11,7 @@ $data = mysqli_fetch_assoc($query);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Profil Saya | RIK MUSIK</title>
+    <title>Profil Saya|RIK MUSIK</title>
 
     <!-- Bootstrap -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
